63687f0fefaf927927151c9ae345259df8fdf92c
Anshika Singh <sighanshika2004@gmail.com>
2024-12-24 18:45:10 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import LandingPage from '../src/components/Landing';
 import ContentGuide from '../src/components/ContentGuide';
-import SignUp from './components/SignUp';
+import SignUp from './components/signUp';
 import Login from './components/Login';
 import AboutUs from './components/AboutUs';
 import Landing from '../src/components/Landing';
